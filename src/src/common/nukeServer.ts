83a668b4cf480c9a4cd68cba5d63a8ea161ed6db
kblauer <kyle.g.blauer@gmail.com>
2023-08-08 06:41:34 -0500

    standardize, fix imports

---
+++ src/src/common/nukeServer.ts
@@ -0,0 +1,19 @@
+import { NS } from '@ns'
+
+export default function nukeServer(ns: NS, hostname: string) : void {
+  // Opens all the ports we can on a server and then nukes it
+
+  ns.tprint(`Nuking server ${hostname}`)
+  if (ns.fileExists("BruteSSH.exe", "home")) {
+    ns.tprint("Running BruteSSH.exe")
+    ns.brutessh(hostname)
+  }
+  if (ns.fileExists("FTPCrack.exe", "home")) {
+    ns.tprint("Running FTPCrack.exe")
+    ns.ftpcrack(hostname)
+  }
+
+  ns.tprint("Running NUKE.exe")
+  ns.nuke(hostname)
+  ns.tprint("Done nuking server")
+}
