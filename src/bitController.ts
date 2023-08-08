ce12037bb2d39e65d285bf2ce651cb1306a57cbf
kblauer <kyle.g.blauer@gmail.com>
2023-08-08 07:54:27 -0500

    create bitController

---
+++ src/bitController.ts
@@ -0,0 +1,24 @@
+import { NS } from '@ns'
+
+export async function main(ns : NS) : Promise<void> {
+  // Controller for the overall management of bitburner gameplay
+
+  // Starting values 
+  const initMoney = ns.getServerMoneyAvailable("home")
+  const initHackLevel = ns.getHackingLevel()
+
+  let currentHackLevel = initHackLevel
+  let lastHackLevel = -100
+  // MAIN APP LOOP
+  while(true) {
+    // run the hacks if our hacking level has increased by 5 or more
+    if (currentHackLevel >= lastHackLevel+5) {
+      ns.run("/src/hack/runAllHacks.js")
+      lastHackLevel = currentHackLevel
+    }
+    
+    // wait 10 minutes
+    await ns.sleep(600000)
+    currentHackLevel = ns.getHackingLevel()
+  }
+}
