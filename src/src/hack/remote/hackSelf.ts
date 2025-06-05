90c95094286971e80bcd61729fd4e6a83c579b20
kblauer <kyle.g.blauer@gmail.com>
2025-06-05 02:43:02 -0500

    don't hack with 0 threads

--- src/src/hack/remote/hackSelf.ts
+++ src/src/hack/remote/hackSelf.ts
@@ -4,7 +4,8 @@ import nukeServer from 'src/common/nukeServer'
 export default function hackSelf(ns : NS, hostname : string) : void {
   // Uploads the payload and execs a hack on the current host (myself)
 
-  const numSelfThreads : number = Math.floor(ns.getServerMaxRam(hostname) / ns.getScriptRam("/src/hack/payload/hackPayload.js"))
+  let numSelfThreads : number = Math.floor(ns.getServerMaxRam(hostname) / ns.getScriptRam("/src/hack/payload/hackPayload.js"))
+  if (numSelfThreads < 1) numSelfThreads = 1
   
   ns.tprint(`Server ${hostname} hacking itself...`)
 
